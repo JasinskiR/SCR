3f6b54ec1798391f67d26b9c54e37656c444a13e
RafalJ7 <259384@student.pwr.edu.pl>
2022-12-31 23:03:20 +0100

    pthread_create main

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,16 +1,22 @@
-#include "Data.hpp"
-#include "PasswdCrack.hpp"
+#include "Threads.hpp"
+#include "Threads.cpp"
 #include <iostream>
-#include <string>
-#include <vector>
 
 using namespace std;
 
 int main() {
   Data hData;
+  Data *ptrhData = &hData;
   hData.readFile();
   vector<string> libWords = readDictionary();
-  //passwdCrack_legacy(hData.hashData, libWords);
-  passwdCrack_No(hData.hashData, libWords);
-  cout << "End of app\n";
+
+  // create threads
+  std::pair<std::vector<std::string>, Data *> p = {libWords, &hData};
+  pthread_create(&listener, &attr, Listener, &ptrhData);
+  pthread_create(&threads[0], &attr, &passwdCrack_legacy, &p);
+  pthread_create(&threads[1], &attr, &passwdCrack_No_prefix, &p);
+  pthread_create(&threads[2], &attr, &passwdCrack_No_sufix, &p);
+  pthread_create(&threads[3], &attr, &passwdCrack_No_prefixANDsufix, &p);
+  pthread_join(listener, nullptr);
+  cout << "__________________________\n";
 }
