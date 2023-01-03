29cebbe958a206dce451fc7639314a9a8a072378
RafalJ7 <259384@student.pwr.edu.pl>
2023-01-03 19:49:14 +0100

    upperCase dictionary and threads

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -5,19 +5,31 @@
 
 using namespace std;
 Data hData;
-void start_threads(pair<vector<string>, Data *> &p, Data *&ptrData) {
+void start_threads(vector<pair<vector<string>, Data *>> &p, Data *&ptrData) {
   pthread_create(&listener, &attr, Listener, &ptrData);
-  pthread_create(&threads[0], &attr, &passwdCrack_legacy, &p);
-  pthread_create(&threads[1], &attr, &passwdCrack_No_prefix, &p);
-  pthread_create(&threads[2], &attr, &passwdCrack_No_sufix, &p);
-  pthread_create(&threads[3], &attr, &passwdCrack_No_prefixANDsufix, &p);
+  pthread_create(&threads[0], &attr, &passwdCrack_legacy, &p[0]);
+  pthread_create(&threads[1], &attr, &passwdCrack_No_prefix, &p[0]);
+  pthread_create(&threads[2], &attr, &passwdCrack_No_sufix, &p[0]);
+  pthread_create(&threads[3], &attr, &passwdCrack_No_prefixANDsufix, &p[0]);
+  pthread_create(&threads[4], &attr, &passwdCrack_doubleWord, &p[0]);
+
+  pthread_create(&threads[5], &attr, &passwdCrack_legacy, &p[1]);
+  pthread_create(&threads[6], &attr, &passwdCrack_No_prefix, &p[1]);
+  pthread_create(&threads[7], &attr, &passwdCrack_No_sufix, &p[1]);
+  pthread_create(&threads[8], &attr, &passwdCrack_No_prefixANDsufix, &p[1]);
+  pthread_create(&threads[9], &attr, &passwdCrack_doubleWord, &p[1]);
+
+  pthread_create(&threads[10], &attr, &passwdCrack_legacy, &p[2]);
+  pthread_create(&threads[11], &attr, &passwdCrack_No_prefix, &p[2]);
+  pthread_create(&threads[12], &attr, &passwdCrack_No_sufix, &p[2]);
+  pthread_create(&threads[13], &attr, &passwdCrack_No_prefixANDsufix, &p[2]);
+  pthread_create(&threads[14], &attr, &passwdCrack_doubleWord, &p[2]);
 }
 
 void stop_threads() {
-  pthread_cancel(threads[0]);
-  pthread_cancel(threads[1]);
-  pthread_cancel(threads[2]);
-  pthread_cancel(threads[3]);
+  for (int i = 0; i <= 15; i++) {
+    pthread_cancel(threads[i]);
+  }
 }
 
 void signal_handler(int signal_num);
@@ -31,13 +43,15 @@ int main(int argc, char *argv[]) {
 
   Data *ptrhData = &hData;
   hData.readFile(argv[1]);
-  vector<string> libWords = readDictionary(argv[2]);
+  vector<string> libWords1 = readDictionary(argv[2]);
+  vector<string> libWords2 = firstUpperCase(libWords1);
+  vector<string> libWords3 = allUpperCase(libWords1);
 
   string new_file;
 
   // create threads
-  std::pair<std::vector<std::string>, Data *> pairV = {libWords, &hData};
-  start_threads(pairV, ptrhData);
+  vector<pair<vector<string>, Data *>> dataVec = {{libWords1, &hData}, {libWords2, &hData}, {libWords3, &hData}};
+  start_threads(dataVec, ptrhData);
 
 
   while (true) {
@@ -46,7 +60,7 @@ int main(int argc, char *argv[]) {
     pthread_mutex_lock(&mutex_);
     hData.readFile(new_file);
     pthread_mutex_unlock(&mutex_);
-    start_threads(pairV, ptrhData);
+    start_threads(dataVec, ptrhData);
   }
 
   cout << "__________________________\n";
